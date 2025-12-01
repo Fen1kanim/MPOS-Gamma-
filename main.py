d3c930df39175a7ba40949c196d755e6f5b0f84f
Fen1k <fen1k.1234567@gmail.com>
2025-12-01 17:53:40 +0100

    main.py can now see the last user from lastUser.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 import json
 from subprocess import call
 with open("keywords.json", "r") as jf:
-    js = json.load(jf)
-
+    keywords = json.load(jf)
+with open('./authentification/lastUser.json', 'r') as js:
+    lastUser = json.load(js)
 call(["python", "./authentification/request.py"])
 
 print("print help to list comands")
@@ -13,11 +14,11 @@ print()
 while True: # main cycle
     stdin = input("--> ")
     [call(["python", "./python/help.py"]) if stdin == 'help' else None] # help
-    [print('Hi! How r u?') for i in js["hi"] if stdin == i] # hi
-    [print('It is', datetime.datetime.now().strftime("%H:%M")) for i in js["time"] if stdin == i] # time
-    [print('Today is', datetime.datetime.now().strftime("%d %B of %y")) for i in js["date"] if stdin == i] # date
+    [print('Hi! How r u?') for i in keywords["hi"] if stdin == i] # hi
+    [print('It is', datetime.datetime.now().strftime("%H:%M")) for i in keywords["time"] if stdin == i] # time
+    [print('Today is', datetime.datetime.now().strftime("%d %B of %y")) for i in keywords["date"] if stdin == i] # date
     [os.system('clear') if stdin == 'clear' or stdin == 'cls' else None] # clear terminal
-    [call(["python", "./python/open.py"]) for i in js["open"] if stdin == i] # open
+    [call(["python", "./python/open.py"]) for i in keywords["open"] if stdin == i] # open
     if stdin == 'exit': # exit to terminal
         print("byeee")
         break
