baf335ab56ea31540fabc80bf60362450e189df9
Fen1k <fen1k.1234567@gmail.com>
2025-12-04 15:53:42 +0100

    deleted all needless modules and opening of an database

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import datetime
-import os
-import json
 from subprocess import call
-
-# opening databases
-with open("keywords.json", "r") as jf:
-    keywords = json.load(jf)
-
-with open('./authentification/lastUser.json', 'r') as js:
-    lastUser = json.load(js)
-
-with open('./authentification/users.json', 'r') as js:
-    users = json.load(js)
-# opening authentification request
-call(["python", "./authentification/request.py"])
 
 # the main programm opens
 print("print help to list comands")
