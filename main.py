0673472a9fb7057eb0263ea55d738e473ea248c0
Fen1k <fen1k.1234567@gmail.com>
2025-12-12 15:15:32 +0100

    added beautiful welcom to mpos text wenn authorizing

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from subprocess import call
 # the main programm opens
 call(["python", './authentification/request.py'])
 
+print('''\n▗▖ ▗▖▗▄▄▄▖▗▖    ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄▖    ▗▄▄▄▖▗▄▖     ▗▖  ▗▖▗▄▄▖  ▗▄▖  ▗▄▄▖
+▐▌ ▐▌▐▌   ▐▌   ▐▌   ▐▌ ▐▌▐▛▚▞▜▌▐▌         █ ▐▌ ▐▌    ▐▛▚▞▜▌▐▌ ▐▌▐▌ ▐▌▐▌
+▐▌ ▐▌▐▛▀▀▘▐▌   ▐▌   ▐▌ ▐▌▐▌  ▐▌▐▛▀▀▘      █ ▐▌ ▐▌    ▐▌  ▐▌▐▛▀▘ ▐▌ ▐▌ ▝▀▚▖
+▐▙█▟▌▐▙▄▄▖▐▙▄▄▖▝▚▄▄▖▝▚▄▞▘▐▌  ▐▌▐▙▄▄▖      █ ▝▚▄▞▘    ▐▌  ▐▌▐▌   ▝▚▄▞▘▗▄▄▞▘
+                                                                          ''')
+
 print("print help to list comands")
 print()
 
