129bbcb973c8ba498a581243497c768e26d6e8f9
Face <fen1k.1234567@gmail.com>
2025-12-29 18:56:47 +0100

    deleted flappybird from menu.py

--- keywords/games/menu.py
+++ keywords/games/menu.py
@@ -4,19 +4,17 @@ from keywords.games.rps import rps
 
 def game():
     stdin = input('''
-1. 2.5D labyrinth
-2. guess a random number
-3. pong
-4. rock paper scissors
-5. snake
-6. tic tac toe
-7. flappy bird \n
-choose a game(1234567): ''')
+0. 2.5D labyrinth
+1. guess a random number
+2. pong
+3. rock paper scissors
+4. snake
+5. tic tac toe\n
+choose a game(012345): ''')
 
-    call(['python', './keywords/games/labyrinth']) if stdin == '1' else None
-    guess() if stdin == "2" else None
-    call(["python", "./keywords/games/pong.py"]) if stdin == '3' else None
-    rps() if stdin == '4' else None
-    call(["python", "./keywords/games/snake.py"]) if stdin == '5' else None
-    call(["python", "./keywords/games/tictactoe.py"]) if stdin == '6' else None
-    call(["python", "./keywords/games/flappy_bird.py"]) if stdin == '7' else None
+    call(['python', './keywords/games/labyrinth']) if stdin == '0' else None
+    guess() if stdin == "1" else None
+    call(["python", "./keywords/games/pong.py"]) if stdin == '2' else None
+    rps() if stdin == '3' else None
+    call(["python", "./keywords/games/snake.py"]) if stdin == '4' else None
+    call(["python", "./keywords/games/tictactoe.py"]) if stdin == '5' else None
