40eaad414e9235e8228b29cf47ccbf5d6ff6d8a2
Face <fen1k.1234567@gmail.com>
2026-01-01 17:00:44 +0100

    renamed authentification/ to auth/

---
+++ auth/authentification.py
@@ -0,0 +1,16 @@
+import json
+
+def auth():
+    with open("authentification/users.json", "r") as js:
+        users = json.load(js)
+    print()
+
+    while True:
+        name = input('Username: ')
+        password = input('Password: ')
+
+        if name in users and password == users[name]["password"]:
+            print('you are as', name, 'authentificated\n')
+            return name
+        else:
+            print('wrong password or username, try again')
